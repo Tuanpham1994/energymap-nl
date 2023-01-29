418ebb78af5b028cd762b6f1369dc2084023ad7c
Pham Anh Tuan <phamtuan.enactusftu@gmail.com>
2023-01-29 13:30:20 +0100

    Initial commit

---
+++ main energy.py
@@ -0,0 +1,15 @@
+###############EDA for energy
+pip install numpy
+pip install pandas
+pip install matplotlib
+pip install seaborn
+
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from datetime import datetime #work with date and time data
+import matplotlib.pyplot as plt #Designing and visualizing
+import matplotlib.cm #Builtin colormaps, colormap handling utilities, and the ScalarMappable mixin.
+from matplotlib import gridspec #A grid layout to place subplots within a figure
+import matplotlib as mpl
+import seaborn as sns #data visulization
